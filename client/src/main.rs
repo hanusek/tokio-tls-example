4ade2b3b395a91e85aa3b31836d4e2c9225ce74d
Michał Hanusek <mhanusek@energotest.com.pl>
2023-01-25 20:38:56 +0100

    Init

---
+++ client/src/main.rs
@@ -0,0 +1,86 @@
+use std::io::{ErrorKind, Error as IoError};
+use std::error::Error;
+use std::result::Result;
+use std::sync::Arc;
+use std::time::SystemTime;
+use std::convert::TryFrom;
+use tokio::net::TcpStream;
+use tokio_rustls::{TlsConnector};
+use tokio_rustls::rustls::{Certificate, ClientConfig, ServerName, RootCertStore};
+use tokio_rustls::rustls::client::{ServerCertVerified, ServerCertVerifier};
+use tokio_rustls::rustls::Error as RustlsError;
+use tokio::io::AsyncWriteExt;
+use tokio::io::AsyncReadExt;
+
+struct DummyVerifier { }
+
+impl DummyVerifier {
+    fn new() -> Self {
+        DummyVerifier { }
+    }
+}
+
+impl ServerCertVerifier for DummyVerifier {
+    fn verify_server_cert(
+        &self, 
+        _end_entity: &Certificate, 
+        _intermediates: &[Certificate], 
+        _server_name: &ServerName, 
+        _scts: &mut dyn Iterator<Item = &[u8]>, 
+        _ocsp_response: &[u8], 
+        _now: SystemTime
+    ) -> Result<ServerCertVerified, RustlsError>
+    {
+        return Ok(ServerCertVerified::assertion()); 
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> 
+{
+    tracing_subscriber::fmt().init();
+
+    let root_cert_store = RootCertStore::empty();
+
+    let mut config = ClientConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_safe_default_protocol_versions()
+        .unwrap()
+        .with_root_certificates(root_cert_store)
+        .with_no_client_auth();
+
+    let dummy_verifier = Arc::new(DummyVerifier::new());
+    config.dangerous().set_certificate_verifier(dummy_verifier);
+
+    const HOST: &str = "127.0.0.1";
+    const PORT: &str = "9900";
+    let addr = format!("{}:{}", HOST, PORT);
+    
+    let connector = TlsConnector::from(Arc::new(config));
+        
+    let domain = ServerName::try_from("xxx")
+        .map_err(|_| IoError::new(ErrorKind::InvalidInput, "invalid dnsname"))?;
+
+    let stream = TcpStream::connect(&addr).await?;
+    let mut tls_stream = connector.connect(domain, stream).await?;
+    
+    tracing::info!("connected: '{:?}", tls_stream);
+    let mut buf = vec![0; 512];
+
+    loop {
+        tls_stream.write_all(b"HELLO").await.expect("failed to write data to socket");
+
+        match tls_stream.read(&mut buf).await
+        {
+            Err(_e) => {},
+            Ok(n) => {
+                if n != 0 { 
+                    let text = String::from_utf8_lossy(&buf[..n]).to_string();
+                    tracing::info!("recv text: {} {:?} from server", n, text);
+                }
+            }
+        };
+    }//loop
+    
+}
---
+++ server/src/main.rs
@@ -0,0 +1,92 @@
+use std::fs::File;
+use std::net::{SocketAddr, TcpStream};
+use std::path::{Path};
+use std::io::{BufReader, ErrorKind, Error as IoError, Result as IoResult};
+use std::error::Error;
+use std::result::Result;
+use std::sync::Arc;
+use std::collections::HashMap;
+
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use tokio_rustls::{TlsAcceptor, TlsStream};
+use tokio_rustls::rustls::{self, Certificate, PrivateKey};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::sync::Mutex;
+
+fn load_certs(path: &Path) -> IoResult<Vec<Certificate>> {
+    certs(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| IoError::new(ErrorKind::InvalidInput, "invalid cert"))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect())
+}
+
+fn load_keys(path: &Path) -> IoResult<Vec<PrivateKey>> {
+    pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| IoError::new(ErrorKind::InvalidInput, "invalid key"))
+        .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> 
+{
+    tracing_subscriber::fmt().init();
+
+    let certs = load_certs(&Path::new("../keys/eddsa-ca.cert"))?;
+    let keys = load_keys(&Path::new("../keys/Ed25519_private_key.pem"))?;
+    
+    let private_key = keys.get(0).unwrap().clone();
+
+    let config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, private_key)
+        .map_err(|err| IoError::new(ErrorKind::InvalidInput, err))?;
+
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+    let listener = TcpListener::bind("0.0.0.0:9900").await?;
+    tracing::info!("listening on port {:?}", listener.local_addr()?);
+
+    let clients_map = Arc::new(Mutex::new(HashMap::<SocketAddr, TlsStream<_>>::new()));
+
+    loop {
+
+        let (stream, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+        let mut tls_stream = acceptor.accept(stream).await?;
+
+        let clients_map_cloned = clients_map.clone();
+
+        tokio::spawn(async move {
+            tracing::info!("connected: ('{}', {})", peer_addr.ip(), peer_addr.port());
+            let mut buf = vec![0; 512];
+            
+            // loop 
+            // {
+                let n = tls_stream
+                        .read(&mut buf)
+                        .await
+                        .expect("failed to read data from socket");
+                
+                let text_msg = String::from_utf8_lossy(&buf[..n]).to_string();
+                tracing::info!("recv bytes: {} {:?} from peer: {:?}", n, text_msg, peer_addr);
+                            
+                {
+                    let mut clients_mutex = clients_map_cloned.lock().await;
+                    tracing::info!("Clients number: {:?}", clients_mutex.len() + 1);
+
+                    // Send to all clients
+                    for (peer_addr, stream) in clients_mutex.iter_mut() 
+                    {
+                        stream.write_all(format!("New client: {:?}", peer_addr.clone()).as_str().as_bytes()).await.expect("failed to write data to socket");
+                        tracing::info!("send msg to peer: {:?}", peer_addr);
+                    }
+
+                    clients_mutex.insert(peer_addr, tokio_rustls::TlsStream::Server(tls_stream));
+                }
+
+            // }//loop internal
+
+        });
+    }
+}
