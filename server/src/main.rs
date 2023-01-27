8ea5fcac1f18ecf0367626d02b7a97a39b23407f
Michał Hanusek <mhanusek@energotest.com.pl>
2023-01-27 19:11:53 +0100

    Problem with borrowing

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,81 +7,51 @@ use std::result::Result;
 use std::collections::HashMap;
 
 use tokio::sync::Mutex;
-use tokio::net::{TcpListener, TcpStream};
-use tokio_rustls::{TlsAcceptor, TlsStream};
+use tokio::net::{TcpListener};
+use tokio_rustls::{TlsAcceptor};
 use tokio_rustls::rustls::ServerConfig;
-use tokio_tungstenite::accept_async;
-use tokio_tungstenite::WebSocketStream;
-use tokio_tungstenite::tungstenite::Message;
-use futures_util::StreamExt;
-use futures_util::SinkExt;
+
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
 
 use certs::{load_keys, load_certs};
 
-async fn handle_ws_connection(peer_addr: SocketAddr, 
-                              tls_stream: TlsStream<TcpStream>,
-                              clients_map: Arc<Mutex<HashMap<SocketAddr, WebSocketStream<TlsStream<TcpStream>>>>>)
+type DataStream = tokio_rustls::server::TlsStream<tokio::net::TcpStream>;
+
+async fn handle_connection(peer_addr: SocketAddr, 
+                           tls_stream: DataStream,
+                           clients_map: Arc<Mutex<HashMap<SocketAddr, DataStream>>>) 
 {
-    let mut ws_stream = accept_async(tls_stream).await.expect("Failed to accept");
+    tracing::info!("New connection: {}", peer_addr);
 
-    tracing::info!("New WebSocket connection: {}", peer_addr);
+    let mut clients_mutex = clients_map.lock().await; 
+    clients_mutex.insert(peer_addr, tls_stream);
 
-    if let Some(msg) = ws_stream.next().await
+    tracing::info!("Clients number: {:?}", clients_mutex.len() + 1);
+
+    const BUFF_SIZE: usize = 512;
+    let mut buf = vec![0; BUFF_SIZE];
+
+    let tls_stream = clients_mutex.get_mut(&peer_addr).unwrap();
+
+    while let Ok(n) = tls_stream.read(&mut buf).await
     {
-        tracing::info!("recv: {:?} from peer: {:?}", msg.unwrap(), peer_addr)
-    }
-
-    {
-        let mut clients_mutex = clients_map.lock().await;
-        tracing::info!("Clients number: {:?}", clients_mutex.len() + 1);
-
-        // Send to all clients
-        for (peer_addr, stream) in clients_mutex.iter_mut() 
-        {
-            let msg = Message::Text(format!("New client: {:?}", peer_addr.clone()));
-            match stream.send(msg.into()).await
-            {
-                Ok(_) => { tracing::info!("send msg to peer: {:?}", peer_addr); },
-                Err(e) =>  { tracing::info!("send error: {:?} to peer: {:?}", e, peer_addr); }
-            };
+        if n == 0 { 
+            tracing::error!("No message from: {}, disconnect!", peer_addr); 
+            return;
         }
 
-        clients_mutex.insert(peer_addr, ws_stream);
+        let text_msg = String::from_utf8_lossy(&buf[..n]).to_string();
+        tracing::info!("recv bytes: {} {:?} from peer: {:?}", n, text_msg, peer_addr);
+        
+        // Send to all clients
+         for (peer_addr, stream) in clients_mutex.iter_mut() 
+         {
+             stream.write_all(format!("New client: {:?}", peer_addr.clone()).as_str().as_bytes()).await.expect("failed to write data to socket");
+             tracing::info!("send msg to peer: {:?}", peer_addr);
+         }
     }
-
 }
-
-// async fn handle_connection(peer_addr: SocketAddr, 
-//                            mut tls_stream: TlsStream<TcpStream>, 
-//                            clients_map: Arc<Mutex<HashMap<SocketAddr, TlsStream<TcpStream>>>>) 
-// {
-//     tracing::info!("New connection: {}", peer_addr);
-//     
-//     const BUFF_SIZE: usize = 512;
-//     let mut buf = vec![0; BUFF_SIZE];
-
-//     let n = tls_stream
-//     .read(&mut buf)
-//     .await
-//     .expect("failed to read data from socket");
-
-//     let text_msg = String::from_utf8_lossy(&buf[..n]).to_string();
-//     tracing::info!("recv bytes: {} {:?} from peer: {:?}", n, text_msg, peer_addr);
-            
-//     {
-//         let mut clients_mutex = clients_map.lock().await;
-//         tracing::info!("Clients number: {:?}", clients_mutex.len() + 1);
-
-//         // Send to all clients
-//         for (peer_addr, stream) in clients_mutex.iter_mut() 
-//         {
-//         stream.write_all(format!("New client: {:?}", peer_addr.clone()).as_str().as_bytes()).await.expect("failed to write data to socket");
-//         tracing::info!("send msg to peer: {:?}", peer_addr);
-//         }
-
-//         clients_mutex.insert(peer_addr, tls_stream);
-//     }
-// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> 
@@ -109,12 +79,12 @@ async fn main() -> Result<(), Box<dyn Error>>
 
         let (stream, peer_addr) = listener.accept().await?;
         let acceptor = acceptor.clone();
-        let tls_stream = acceptor.accept(stream).await?;
-
+        let mut tls_stream = acceptor.accept(stream).await?;
+        
         let clients_map_cloned = clients_map.clone();
 
         tokio::spawn(async move {
-            handle_ws_connection(peer_addr, tokio_rustls::TlsStream::Server(tls_stream), clients_map_cloned).await;
+            handle_connection(peer_addr, tls_stream, clients_map_cloned).await;
         });
     }
 }
